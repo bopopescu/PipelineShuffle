4b3248ee087a69ac3833818a20e5e1a26d140712
Guanhua Wang <alexwgh333@gmail.com>
2017-07-15 16:27:13 -0700

    Update execute.sh

--- conf/execute.sh
+++ conf/execute.sh
@@ -6,4 +6,3 @@ while [ $SECONDS -lt $end ]; do
     echo `date +%s%3N` `cat /sys/class/net/eth0/statistics/rx_bytes` `cat /sys/class/net/eth0/statistics/tx_bytes` \
       `cat <(grep 'cpu ' /proc/stat) <(sleep 0.3 && grep 'cpu ' /proc/stat) | awk -v RS="" '{print ($13-$2+$15-$4)*100/(1+$13-$2+$15-$4+$16-$5)}'`
 done
-
